022b55f881b95ae1fb92709860a0697c3bbd43f3
Krys <aslay.k@qq.com>
2017-08-20 19:45:49 +0800

    test

--- OperationUnit/canvasView.cc
+++ OperationUnit/canvasView.cc
@@ -19,29 +19,22 @@ void canvasView::init()
 {
     qDebug() << "canvasView::init()";
     scene_ = std::make_unique<canvasScene>(QRectF{0, 0, 1500, 1200});
-//    this->setScene(scene_.get());
     auto ptr_scene = scene_.get();
     this->setScene(ptr_scene);
     setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
 
-//    setRenderHints (QPainter::SmoothPixmapTransform);
     setRenderHints (QPainter::Antialiasing);
 
+    auto item1 = new assemblyArea;
+    item1->setPos (100, 100);
 
-//    auto process = new processingZone;
-//    scene_->addItem(process);
-//    auto assembly = new assemblyArea;
-//    scene_->addItem(assembly);
-//    auto transport = new transportArea;
-//    scene_->addItem(transport);
-//    auto storage = new storageArea;
-//    scene_->addItem(storage);
-//    auto check = new checkingArea;
-//    scene_->addItem(check);
-//    auto office = new officeArea;
-//    scene_->addItem(office);
-//    auto staging = new stagingArea;
-//    scene_->addItem(staging);
-//    auto service = new serviceArea;
-//    scene_->addItem(service);
+    auto item2 = new assemblyArea;
+    item1->setPos (200, 300);
+
+    scene ()->addItem (item1);
+    scene ()->addItem (item2);
+
+
+    auto line = new LineA (item1, item2);
+    scene ()->addItem (line);
 }
